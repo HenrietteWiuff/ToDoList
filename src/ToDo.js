e318d4b525527b76f1c5ee0ea0265e4af638a355
HenrietteWiuff <enni_wiuff@hotmail.com>
2021-02-26 12:20:34 +0100

    Responsiv on 3 devices
    Color changes for all 3 devices

--- src/ToDo.js
+++ src/ToDo.js
@@ -3,14 +3,14 @@ import React from 'react';
 const ToDo = ({todo, handleToggle}) => {
 
     const handleClick = (e) => {
-        e.preventDefault()
+        // e.preventDefault()
         handleToggle(e.currentTarget.id)
     }
 
    return (
        
        <div id={todo.id} className={todo.complete ? "strike" : ""} onClick={handleClick}>
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" id={todo.id} className={todo.complete ? "flexCheckChecked" : ""}></input>
+        <input class="form-check-input" type="checkbox" value="" checked={todo.complete} id={todo.id} ></input>
            {todo.task}
        </div>
    );
